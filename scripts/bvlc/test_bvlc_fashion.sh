18750f638dabf8b1937460a2e833a8e7b7fed08b
zhaoqc418 <751019435@qq.com>
2021-12-06 20:04:03 +0800

    new update

--- scripts/bvlc/test_bvlc_fashion.sh
+++ scripts/bvlc/test_bvlc_fashion.sh
@@ -1,10 +1,11 @@
-config=lgcn/configs/cfg_test3_lgcn_ms1m.py
-load_from=data/pretrained_models/pretrained_lgcn_ms1m.pth
+config=bvlc/configs/cfg_test_bvlc_fashion.py
+load_from=data/pretrained_models/pretrained_bvlc_fashion.pth
+
 
 export CUDA_VISIBLE_DEVICES=0
 export PYTHONPATH=.
 
-python lgcn/main.py \
+python bvlc/main.py \
     --config $config \
     --phase 'test' \
     --load_from $load_from \
--- scripts/bvlc/test_bvlc_ms1m.sh
+++ scripts/bvlc/test_bvlc_ms1m.sh
@@ -1,11 +1,10 @@
-config=lgcn/configs/cfg_test_lgcn_fashion.py
-load_from=data/pretrained_models/pretrained_lgcn_fashion.pth
-
+config=bvlc/configs/cfg_test_bvlc_ms1m.py
+load_from=data/pretrained_models/pretrained_bvlc_ms1m.pth
 
 export CUDA_VISIBLE_DEVICES=0
 export PYTHONPATH=.
 
-python lgcn/main.py \
+python bvlc/main.py \
     --config $config \
     --phase 'test' \
     --load_from $load_from \
